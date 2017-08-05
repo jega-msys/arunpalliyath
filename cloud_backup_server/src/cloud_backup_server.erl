0d73ae7ab6fdd34290398d26b471988d00f670c4
Arun P <arunpal356@gmail.com>
2017-08-05 15:08:22 +0530

    Add directory structure and necessary files

---
+++ cloud_backup_server/src/cloud_backup_server.erl
@@ -0,0 +1,6 @@
+%% -------------------------------------------------------------
+%% 	Module contains functions which provides get, post and list
+%%	Functionalities.
+%% -------------------------------------------------------------
+-module(cloud_backup_server).
+-export([ ]).
---
+++ cloud_backup_server/test/cloud_backup_server_tests.erl
@@ -0,0 +1,5 @@
+%% -------------------------------------------------------------
+%% 	Module contains test cases to test get, post and list
+%%	Functionalities.
+%% -------------------------------------------------------------
+-module(cloud_backup_server_tests).
